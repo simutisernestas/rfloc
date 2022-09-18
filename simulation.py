9ac9b9ca70657869f004698e9b2e0df6779a162b
Ernestas <simutisernestas@gmail.com>
2022-09-18 15:39:23 +0200

    switch to EKF

--- simulation.py
+++ simulation.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from scipy.optimize import least_squares
 from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+from filterpy.kalman import ExtendedKalmanFilter
 from typing import List
+
 
 class Beacon:
 
@@ -14,6 +17,10 @@ class Beacon:
 
     def get_pos(self) -> np.array:
         return self.__x
+
+
+def dist(a, b):
+    return np.linalg.norm(a - b, 2)
 
 
 class Agent:
@@ -29,13 +36,15 @@ class Agent:
     def get_state(self) -> np.array:
         return self.__x
 
+    def get_beacon_dists(self) -> np.array:
+        return np.array(
+            [dist(agent.get_state()[:3], b.get_pos()) + np.random.random() for b in beacons])
+
     def triangulate_pos(self, beacons: List[Beacon]):
         A = np.zeros((3, 3))
 
-        def dist(a, b):
-            return np.linalg.norm(a - b, 2)
-        ds = np.array(  # distances to beacons (ground truth)
-            # [dist(agent.get_state()[:3], b.get_pos())for b in beacons])
+        ds = np.array(  # distances to beacons
+            # [dist(agent.get_state()[:3], b.get_pos())for b in beacons]) <= (ground truth)
             [dist(agent.get_state()[:3], b.get_pos()) + np.random.random() for b in beacons])
 
         bp = [b.get_pos() for b in beacons]
@@ -65,7 +74,18 @@ class Agent:
 
         return np.linalg.inv(2*A) @ b
 
-    def advance(self, F) -> None:
+    def advance(self) -> None:
+        # linear motion model
+        F = np.eye(9)
+        F[0][3] = dt
+        F[1][4] = dt
+        F[2][5] = dt
+        F[0][6] = .5*dt**2
+        F[1][7] = .5*dt**2
+        F[2][8] = .5*dt**2
+        F[3][6] = dt
+        F[4][7] = dt
+        F[5][8] = dt
         self.__x = F @ self.__x
 
 
@@ -115,23 +135,20 @@ def dist_jac(x_op: np.ndarray, beacons: List[Beacon]):
     -(bx - x)/((bx - x)^2 + (by - y)^2 + (bz - z)^2)^(1/2)
     -(by - y)/((bx - x)^2 + (by - y)^2 + (bz - z)^2)^(1/2)
     -(bz - z)/((bx - x)^2 + (by - y)^2 + (bz - z)^2)^(1/2)
+    or switch bx,x places and remove minus in front
 
     Parameters
     ----------
-    x_op : np.ndarray
-        operating point (3,1), at the same time it's agent 
-        previous position.
+    x_op : np.ndarray (3,1)
+        operating point i.e. agent's previous position.
     """
-    # pr = agent.get_state()[:3]
-    H = np.zeros((len(beacons),3))
-    for i,b in enumerate(beacons):
+    H = np.zeros((len(beacons), 3))
+    for i, b in enumerate(beacons):
         H[i] = ((x_op - b.get_pos()) / np.linalg.norm(x_op - b.get_pos())).T
     return H
 
 
 if __name__ == '__main__':
-
-
     def gen_beacon_on_the_groud():
         # point
         r = np.random.random((3, 1))
@@ -141,80 +158,63 @@ if __name__ == '__main__':
         return Beacon(x0=r)
     beacons = [gen_beacon_on_the_groud() for _ in range(4)]
 
-    H = dist_jac(np.ones((3,1)), beacons)
-    print(H)
-    exit()
-
-
     ax0 = np.zeros((9, 1))
     ax0[:3] = (np.random.random((3, 1)) - .5) * 50
     if ax0[2] < 0:  # above ground
         ax0[2] *= -1
-    ax0[3] = 10
+    ax0[3] = 10  # go right at 10 m/s
     agent = Agent(x0=ax0.copy())
 
-    f = KalmanFilter(dim_x=9, dim_z=3)
-    f.x = agent.get_state()
-
-    # Time step
     dt = 1e-2
-
-    # The initial uncertainty (9x9).
-    f.P = np.eye(9, 9) * 1000
-
-    # The external motion (9x1).
-    u = np.zeros((9, 1))
-
-    # The transition matrix (9x9).
-    F = np.eye(9)
-    F[0][3] = dt
-    F[1][4] = dt
-    F[2][5] = dt
-    F[0][6] = .5*dt**2
-    F[1][7] = .5*dt**2
-    F[2][8] = .5*dt**2
-    F[3][6] = dt
-    F[4][7] = dt
-    F[5][8] = dt
-    f.F = F
-
-    # The observation matrix (3x9).
-    f.H = np.zeros((3, 9))
-    f.H[0][0] = 1
-    f.H[1][1] = 1
-    f.H[2][2] = 1
-
-    # The measurement uncertainty.
-    f.R *= 10
 
     path = []
     gt_path = []
+
+    # build filter
+    rk = ExtendedKalmanFilter(dim_x=3, dim_z=4)
+    # noisy initial guess
+    rk.x = agent.get_state()[:3] + np.random.random((3, 1))*3
+    rk.F = np.eye(3)  # F TODO: include velocity
+    range_std = 5.  # meters
+    rk.R = np.diag([range_std**2]*4)  # TODO: measurement noise
+    rk.P *= 50  # TODO: variance
+
+    def HJacobian_at(x, beacons):
+        return dist_jac(x[:3], beacons)
+
+    def hx(x, beacons):
+        h = np.zeros((4, 1))
+        for i, b in enumerate(beacons):
+            h[i] = np.linalg.norm(x[:3] - b.get_pos())
+        return h
 
     for i in range(4):
         counter = 0
         while True:
             # ground truth based on physics
-            agent.advance(F)
+            agent.advance()
             gt_path.append(agent.get_state()[:2])
             if counter > 100:
                 break
             counter += 1
-            f.predict()
-            Z = agent.triangulate_pos(beacons)
-            f.update(Z)
-            path.append(f.x)
+            z = agent.get_beacon_dists()
+            rk.update(np.array([z]).T, HJacobian_at, hx,
+                      args=(beacons), hx_args=(beacons))
+            path.append(rk.x)
+            rk.predict()
         state = agent.get_state().copy()
         state[3:6] = 0
-        if i == 0:
+        if i == 0:  # go up at 10 m/s
             state[4] = 10
-        elif i == 1:
-            state[3] = -20
-        elif i == 2:
+        elif i == 1:  # go left at 12 m/s
+            state[3] = -12
+        elif i == 2:  # go down at 10 m/s
             state[4] = -10
         agent.update(state)
 
-    print(f"||X - GT|| = {np.linalg.norm(f.x[:3] - agent.get_state()[:3])}")
-    print(agent.get_state(), '\n\n', f.x)
+    print(f"Var: {np.max(rk.P)}\n"
+          f"Dist: {np.linalg.norm(rk.x[:3] - agent.get_state()[:3])}\n")
+    print(agent.get_state(), '\n\n', rk.x)
 
     path = np.array(path)
     gt_path = np.array(gt_path)
