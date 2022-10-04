4da302e40764b4d7c6a7b15c1fe8a5d4c66b9288
Ernie <simutisernestas@gmail.com>
2022-10-04 12:06:33 +0200

    TODOs update

---
+++ varest.py
@@ -0,0 +1,9 @@
+"""TODO: implement variance estimation experiment code!
+ 
+ Run indefinitely, until got exit input:
+    1) take in distance
+    2) run until collected N samples
+    3) put in a csv file for instance, column name is GT distance, rows are measured values
+       or just new file per one GT distance and write measurements there, not to fuck up 
+       a single file in case of any errors : )
+"""
