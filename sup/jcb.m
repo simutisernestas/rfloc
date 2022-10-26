ae5dfc6b5070a63ffdcc69bf013251b9b230be2b
Ernestas <simutisernestas@gmail.com>
2022-10-26 20:14:53 +0200

    update report

--- sup/jcb.m
+++ sup/jcb.m
@@ -1,17 +1,17 @@
 clc
-clear all
+clear
 
 syms x y z bx by bz
 
 pr = [x;y;z];
 pb = [bx;by;bz];
 
-h = norm(pr - pb); % for single beacon
+h = sqrt(sum((pr-pb).^2)) %norm(pr-pb,2) % for single beacon
+dh = simplify(diff(h))
+
 J = jacobian(h,[x,y,z]);
 
-
-beacons
-
+% beacons
 
 % op_x = [0,0];
 % subs(J, [x1,x2], op_x)
