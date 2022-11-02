88585e6d148a248e148de3d1b74334a33c1bce61
Ernestas <s212571@student.dtu.dk>
2022-11-02 15:34:46 +0100

    cov plotting & param optim

--- experiment.py
+++ experiment.py
@@ -3,7 +3,8 @@ from rfloc import *
 from bagpy import bagreader
 import pandas as pd
 
-RANGE_MEAS_LIFESPAN = .2
+RANGE_MEAS_LIFESPAN = 5e-2
+NZ_FOR_UPDATE = 3
 BEACON_MAP = {
     "20725": Beacon("20725", np.array([-1.51, 3.32, 1.05]), lifespan=RANGE_MEAS_LIFESPAN),
     "52422": Beacon("52422", np.array([4.22, 2.38, .7]), lifespan=RANGE_MEAS_LIFESPAN),
@@ -14,10 +15,8 @@ BAG_NAME = "2022-10-31-15-05-16.bag"
 
 if __name__ == '__main__':
     DIM_X = 6
-    DIM_Z = 4
-    Pnoise = 3e-3
-    R = np.diag([Pnoise]*DIM_Z)
-    P = np.eye(DIM_X)*10
+    Pnoise = 3e-4*4
+    P = np.eye(DIM_X)*5e-2
 
     fdev = 2.0
     path = []
@@ -26,7 +25,7 @@ if __name__ == '__main__':
 
     agent = Agent()
     ax0 = np.zeros((DIM_X, 1))
-    b = bagreader(BAG_NAME)
+    b = bagreader(BAG_NAME, verbose=False)
     pose_csv = b.message_by_topic("/vrpn_client_node/awww/pose")
     df = pd.read_csv(pose_csv)
     df = df.reset_index()
@@ -43,7 +42,7 @@ if __name__ == '__main__':
         [df["pose.position.x"], df["pose.position.y"], df["pose.position.z"]]).T
     gt_path = gt_path[:int(gtlen/fdev), :]
 
-    b = bagreader(BAG_NAME)
+    b = bagreader(BAG_NAME, verbose=False)
     chatter_csv = b.message_by_topic("/chatter")
     df = pd.read_csv(chatter_csv)
     df = df.reset_index()
@@ -56,35 +55,28 @@ if __name__ == '__main__':
             print("NOT IN THE MAP! : O")
             continue
         BEACON_MAP[beacon_id].update_range(meas_range, stamp)
-        # print(f"{stamp}; Beacon: {beacon_id}; Range {meas_range}")
         measurements = [v.get_range(stamp) for k, v in BEACON_MAP.items()]
         z = []
         idxs = []
-        for i,m in enumerate(measurements):
+        for i, m in enumerate(measurements):
             if m is not None:
                 z.append(m)
                 idxs.append(i)
-        # got_4_meas = True if None not in measurements else False
-        # if got_4_meas:
         last_update = agent.get_last_update()
         dt = stamp - last_update
-        #  and dt > RANGE_MEAS_LIFESPAN
-        if (len(z) > 2) or len(z) == 4:
+        x = agent.get_state()
+        (x, P) = predict(x, P, getF(dt), 0, Pnoise)
+        if len(z) >= NZ_FOR_UPDATE:
             obs_beacons = [list(BEACON_MAP.values())[i] for i in idxs]
-            for b in obs_beacons:
-                b.discard_meas()
-            x = agent.get_state()
-            (x, P) = predict(x, P, getF(dt), 0, Pnoise)
             z = np.array(z).reshape((len(z), 1))
-            if (abs(z) > 10).any():
+            if (abs(z) > 15).any():
                 continue
-            # (x, P) = update(x, hx(x, BEACON_MAP.values()),
-            #                 P, z, getH(x, BEACON_MAP.values()), R)
+            R = np.diag([Pnoise]*len(z))
             (x, P) = update(x, hx(x, obs_beacons),
-                            P, z, getH(x, obs_beacons), np.diag([Pnoise]*len(z)))
-            path.append(x)
-            Pss.append(P)
-            agent.update(x, stamp)
+                            P, z, getH(x, obs_beacons), R)
+        agent.update(x, stamp)
+        Pss.append(P)
+        path.append(x)
 
     print(f"Var: {np.max(P)}\n"
           f"Pos: {agent.get_state()[:3].T}")
@@ -92,7 +84,8 @@ if __name__ == '__main__':
     path = np.array(path)
     pathlen = path.shape[0]
     path = path[:int(pathlen/fdev), :]
-
     Pss = Pss[:int(pathlen/fdev)]
 
-    mapp(BEACON_MAP.values(), ax0, path, gt_path, Pss, threed=True)    
+    plot_gt_vs_data_with_cov(path, gt_path, Pss, BEACON_MAP.values())
+    mapp(BEACON_MAP.values(), ax0, path, gt_path, Pss, threed=True)
+    plt.show()
--- rfloc.py
+++ rfloc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 
 
 class Beacon:
@@ -42,6 +43,7 @@ class Beacon:
 
     def discard_meas(self):
         self.__range = None
+
 
 class Agent:
 
@@ -104,60 +106,76 @@ class Agent:
         self.__x = F @ self.__x
 
 
-def mapp(beacons: list, ax0: np.ndarray, path: np.ndarray, gt_path: np.ndarray = None, Ps = None, threed=False):
+def mapp(beacons: list, ax0: np.ndarray, path: np.ndarray, gt_path: np.ndarray = None, Ps=None, threed=False):
     plt.figure(1, dpi=150)
-    # legends = []
-    # plt.plot(path[:, 0].T[0], path[:, 1].T[0])
-    # legends.append("Path")
+    plt.tight_layout()
+    plt.axis('equal')
+    legends = []
+    plt.plot(path[:, 0].T[0], path[:, 1].T[0])
+    legends.append("Path")
 
-    # for i, b in enumerate(beacons):
-    #     x = b.get_pos()
-    #     plt.scatter(x[0], x[1], s=100)
-    #     legends.append("Beacon " + str(i))
-    # plt.scatter(ax0[0], ax0[1], marker='x', s=100)
-    # legends.append("x0")
+    for i, b in enumerate(beacons):
+        x = b.get_pos()
+        plt.scatter(x[0], x[1], s=100)
+        legends.append("Beacon " + str(i))
+    plt.scatter(ax0[0], ax0[1], marker='x', s=100)
+    legends.append("x0")
 
-    # if gt_path is not None:
-    #     plt.plot(gt_path[:, 0], gt_path[:, 1])
-    # legends.append("GT")
-    # plt.legend(legends)
+    if gt_path is not None:
+        plt.plot(gt_path[:, 0], gt_path[:, 1])
+    legends.append("GT")
+    plt.legend(legends)
 
-    # if threed:
-    #     plt.figure(2, dpi=150)
-    #     ax = plt.axes(projection='3d')
-    #     ax.plot3D(path[:, 0].T[0], path[:, 1].T[0], path[:, 2].T[0])
-    #     # legends.append("Path")
+    if threed:
+        plt.figure(2, dpi=150)
+        ax = plt.axes(projection='3d')
+        ax.plot3D(path[:, 0].T[0], path[:, 1].T[0], path[:, 2].T[0])
+        # legends.append("Path")
 
-    #     for i, b in enumerate(beacons):
-    #         x = b.get_pos()
-    #         ax.scatter3D(x[0], x[1], x[2], s=100)
-    #         legends.append(str(i))
-    #     ax.scatter3D(ax0[0], ax0[1], ax0[2], marker='x', s=100)
-    #     # legends.append("x0")
+        for i, b in enumerate(beacons):
+            x = b.get_pos()
+            ax.scatter3D(x[0], x[1], x[2], s=100)
+            legends.append(str(i))
+        ax.scatter3D(ax0[0], ax0[1], ax0[2], marker='x', s=100)
+        # legends.append("x0")
 
-    #     if gt_path is not None:
-    #         ax.plot3D(gt_path[:, 0], gt_path[:, 1], gt_path[:, 2])
-    #     # legends.append("GT")
-    #     ax.legend(legends)
+        if gt_path is not None:
+            ax.plot3D(gt_path[:, 0], gt_path[:, 1], gt_path[:, 2])
+        # legends.append("GT")
+        ax.legend(legends)
 
-    if Ps is not None:
-        from matplotlib.patches import Ellipse
-        for i,P in enumerate(Ps):
-            P = P[:2,:2]
-            lambda_, v = np.linalg.eig(P)
-            ax = plt.subplot(111, aspect='equal')
-            nstd = 3
-            w, h = 2 * nstd * np.sqrt(lambda_)
-            ell = Ellipse(xy=(path[i,0],path[i,1]),
-                        width=w, height=h,
-                        angle=np.rad2deg(np.arccos(v[0, 0])), color='black',
-                        linestyle='--')
-            ell.set_facecolor('none')
-            ax.add_artist(ell)
+
+def plot_gt_vs_data_with_cov(path, gt, Ps, beacons):
+    plt.figure(123, dpi=150)
+    legends = []
+
+    ax = plt.subplot(111, aspect='equal')
     ax.plot(path[:, 0].T[0], path[:, 1].T[0])
-    ax.plot(gt_path[:, 0], gt_path[:, 1])
+    legends.append("Path")
 
-    plt.show()
+    for i, b in enumerate(beacons):
+        x = b.get_pos()
+        ax.scatter(x[0], x[1], s=100)
+        legends.append("Beacon " + str(i))
+
+    if gt is not None:
+        ax.plot(gt[:, 0], gt[:, 1])
+    legends.append("GT")
+    ax.legend(legends)
+
+    for i, P in enumerate(Ps):
+        if i % 11 != 0:
+            continue
+        P = P[:2, :2]
+        lambda_, v = np.linalg.eig(P)
+        nstd = 3
+        w, h = 2 * nstd * np.sqrt(lambda_)
+        ell = Ellipse(xy=(path[i, 0], path[i, 1]),
+                      width=w, height=h,
+                      angle=np.rad2deg(np.arccos(v[0, 0])), color='black',
+                      linestyle='--')
+        ell.set_facecolor('none')
+        ax.add_artist(ell)
 
 
 def update(x, hx, P, Z, H, R):
